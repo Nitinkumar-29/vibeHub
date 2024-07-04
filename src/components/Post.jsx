ac4ea7e5ccbe895fbba848edd587110205864b45
nitin <nimble2405@gmail.com>
2024-07-04 15:02:22 +0530

    added like dislike and comment deletion functionality

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,9 @@
 import {
   addDoc,
+  arrayRemove,
+  arrayUnion,
   collection,
+  deleteDoc,
   doc,
   getDoc,
   getDocs,
@@ -19,9 +22,10 @@ import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 import { SlBubble, SlHeart, SlPaperPlane } from "react-icons/sl";
 import { formatDistanceToNow } from "date-fns";
-import { BsSave } from "react-icons/bs";
+import { BsHeartFill, BsSave } from "react-icons/bs";
 import { RxPaperPlane } from "react-icons/rx";
 import { BiLoader } from "react-icons/bi";
+import { FiTrash2 } from "react-icons/fi";
 
 const Post = () => {
   const [postData, setPostData] = useState([]);
@@ -33,37 +37,54 @@ const Post = () => {
   const [isPublished, setIsPublished] = useState(true);
   const userData = JSON.parse(localStorage.getItem("loggedInUserData"));
 
-  userData && console.log(userData);
-  useEffect(() => {
-    const fetchPostById = async (id) => {
-      try {
-        const postRef = doc(db, "posts", id);
-        const postSnapShot = await getDoc(postRef);
+  const fetchPostById = async (id) => {
+    try {
+      const postRef = doc(db, "posts", id);
+      const postSnapShot = await getDoc(postRef);
 
-        if (postSnapShot.exists()) {
-          const postData = postSnapShot.data();
-          console.log("Post data: ", postData);
-          // You can set the post data to state if needed
-          setPostData(postData);
-        } else {
-          console.log("No such document!");
-        }
-      } catch (error) {
-        console.error("Error fetching post: ", error);
+      if (postSnapShot.exists()) {
+        const postData = postSnapShot.data();
+        console.log("Post data: ", postData);
+        // You can set the post data to state if needed
+        setPostData(postData);
+      } else {
+        console.log("No such document!");
       }
-    };
-
-    if (currentUser?.uid) {
-      fetchPostById(id);
+    } catch (error) {
+      console.error("Error fetching post: ", error);
     }
-    // eslint-disable-next-line
-  }, [id, currentUser?.uid]);
+  };
 
   const formatDate = (timestamp) => {
     const date = new Date(
       timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
     );
     return formatDistanceToNow(date, { addSuffix: true });
+  };
+  const fetchPostComments = async () => {
+    try {
+      const q = query(
+        collection(db, "postComments"),
+        where("postId", "==", id)
+      );
+      const querySnapshot = await getDocs(q);
+      const comments = [];
+      querySnapshot.forEach((doc) => {
+        comments.push({ id: doc.id, ...doc.data() });
+        console.log(comments);
+      });
+      // Sort comments by timestamp
+      comments.sort((a, b) => {
+        if (a.timestamp && b.timestamp) {
+          return b.timestamp.seconds - a.timestamp.seconds;
+        } else {
+          return 0; // Handle cases where timestamp might be missing
+        }
+      });
+      setPostComments(comments);
+    } catch (error) {
+      console.error("Error fetching comments: ", error);
+    }
   };
 
   const handlePostComment = async (e) => {
@@ -97,30 +118,118 @@ const Post = () => {
       console.error(error);
       setIsPublished(false);
     }
-
+    fetchPostById(id);
     fetchPostComments();
+    setPostData({
+      commentsCount: postComment.length,
+    });
   };
 
-  const fetchPostComments = async () => {
+  const handleDeleteComment = async (commentId) => {
     try {
-      const q = query(
-        collection(db, "postComments"),
-        where("postId", "==", id)
-      );
-      const querySnapshot = await getDocs(q);
-      const comments = [];
-      querySnapshot.forEach((doc) => {
-        comments.push({ id: doc.id, ...doc.data() });
-        console.log(comments);
+      const commentRef = doc(db, "postComments", commentId);
+      await deleteDoc(commentRef);
+      fetchPostComments();
+      console.log(`Comment with ID ${commentId} deleted`);
+      // Increment the comment count in the corresponding post document
+      const postRef = doc(db, "posts", id);
+      await updateDoc(postRef, {
+        commentsCount: increment(-1),
       });
-      setPostComments(comments);
+      fetchPostById(id);
     } catch (error) {
-      console.error("Error fetching comments: ", error);
+      console.error("Error deleting comment: ", error);
     }
   };
+  const handleLikePost = async () => {
+    try {
+      const postRef = doc(db, "posts", id);
+      const postSnap = await getDoc(postRef);
+      console.log(postSnap);
+      if (postSnap.exists()) {
+        const postData = postSnap.data();
+
+        // Ensure postData.likes is initialized properly
+        const likes = postData.likes || [];
+
+        if (likes.includes(currentUser.uid)) {
+          await updateDoc(postRef, {
+            likes: arrayRemove(currentUser.uid),
+            likesCount: increment(-1),
+          });
+          setPostData((prevData) => ({
+            ...prevData,
+            likes: prevData.likes.filter((uid) => uid !== currentUser.uid),
+            likesCount: prevData.likesCount - 1,
+          }));
+          console.log("post disliked");
+          fetchPostById(id);
+        } else {
+          await updateDoc(postRef, {
+            likes: arrayUnion(currentUser.uid),
+            likesCount: increment(1),
+          });
+          setPostData((prevData) => ({
+            ...prevData,
+            likes: [...prevData.likes, currentUser.uid],
+            likesCount: prevData.likesCount + 1,
+          }));
+          console.log("post liked");
+
+          fetchPostById(id);
+        }
+      }
+    } catch (error) {
+      console.error("Error liking post: ", error);
+    }
+  };
+
+  const handleLikeComment = async (commentId) => {
+    try {
+      const commentRef = doc(db, "postComments", commentId);
+      const commentSnap = await getDoc(commentRef);
+
+      if (commentSnap.exists()) {
+        const postData = commentSnap.data();
+        if (postData.likes.includes?.(currentUser.uid)) {
+          await updateDoc(commentRef, {
+            likes: arrayRemove(currentUser.uid),
+            likesCount: increment(-1),
+          });
+          setPostData((prevData) => ({
+            ...prevData,
+            likes: [...prevData.likes, currentUser.uid],
+            likesCount: prevData.likesCount - 1,
+          }));
+        } else {
+          await updateDoc(commentRef, {
+            likes: arrayUnion(currentUser.uid),
+            likesCount: increment(1),
+          });
+          setPostData((prevData) => ({
+            ...prevData,
+            likes: [...prevData.likes, currentUser.uid],
+            likesCount: prevData.likesCount + 1,
+          }));
+          await fetchPostComments();
+        }
+      }
+    } catch (error) {
+      console.error("Error liking post: ", error);
+    }
+  };
+
   useEffect(() => {
     fetchPostComments();
+    // eslint-disable-next-line
   }, [id]);
+
+  useEffect(() => {
+    if (currentUser?.uid) {
+      fetchPostById(id);
+    }
+    // eslint-disable-next-line
+  }, [id, currentUser?.uid]);
 
   return (
     <div
@@ -196,10 +305,30 @@ const Post = () => {
                 </Carousel>
                 <div className="flex items-center justify-between h-10 p-2">
                   <div className="flex items-center space-x-6">
-                    <SlHeart size={20} />
+                    <div className="flex items-center space-x-1">
+                      <span onClick={handleLikePost}>
+                        {postData.likes.includes(currentUser.uid) ? (
+                          <BsHeartFill
+                            size={20}
+                            className="text-red-600 cursor-pointer"
+                          />
+                        ) : (
+                          <SlHeart
+                            cursor={PointerEvent}
+                            className="cursor-pointer"
+                            size={20}
+                          />
+                        )}
+                      </span>
+                      {postData.likes.length >= 0 && (
+                        <span>{postData.likes.length}</span>
+                      )}
+                    </div>
                     <div className="flex items-center space-x-1">
                       <SlBubble size={20} />
-                      <span> {postData.commentsCount}</span>
+                      {postComments.length >= 0 && (
+                        <span>{postComments.length}</span>
+                      )}
                     </div>
                     <SlPaperPlane size={20} />
                   </div>
@@ -249,35 +378,60 @@ const Post = () => {
                 <div className="flex flex-col space-y-2 w-full">
                   {postComments.map((comment) => {
                     return (
-                      <>
-                        <div className="flex flex-col space-y-2 p-2 border-[1px] rounded-md w-full">
-                          <div className="flex w-full justify-between items-center">
-                            <div className="flex items-center space-x-1">
-                              {comment.userProfileImage ? (
-                                <img
-                                  src={comment.userProfileImage}
-                                  className="h-8 w-8 rounded-full border-[1px]"
-                                  alt=""
-                                />
-                              ) : (
-                                <FaUser className="h-8 w-8 border-[1px] rounded-full border-blue-800" />
-                              )}
-                              <span>{comment.name}</span>
+                      <div
+                        key={comment.id}
+                        className="flex flex-col space-y-2 p-2 border-[1px] rounded-md w-full"
+                      >
+                        <div className="flex w-full items-center justify-between">
+                          {
+                            <div className="flex w-full justify-between items-center">
+                              <div className="flex items-center space-x-1">
+                                {comment.userProfileImage ? (
+                                  <img
+                                    src={comment.userProfileImage}
+                                    className="h-8 w-8 rounded-full border-[1px]"
+                                    alt=""
+                                  />
+                                ) : (
+                                  <FaUser className="h-8 w-8 border-[1px] rounded-full border-blue-800" />
+                                )}
+                                {postData.userId === comment.userId ? (
+                                  <div className="flex items-center space-x-1">
+                                    <span>{comment.name}</span>{" "}
+                                    <span className="text-zinc-500">
+                                      (author)
+                                    </span>
+                                  </div>
+                                ) : (
+                                  <span>{comment.name}</span>
+                                )}
+                              </div>
+                              <span>
+                                {comment.timeStamp &&
+                                  formatDate(comment.timeStamp, "PPpp")}
+                              </span>
                             </div>
-                            <span>
-                              {comment.timeStamp &&
-                                formatDate(comment.timeStamp, "PPpp")}
-                            </span>
-                          </div>
-                          <div className="tracking-tighter">
-                            {comment.comment}
-                          </div>
-                          <div className="flex items-center w-full space-x-4">
-                            <SlHeart size={15} />
-                            <SlBubble size={15} />
-                          </div>
+                          }
+                          {(currentUser.uid === postData.userId ||
+                            currentUser.uid === comment.userId) && (
+                            <FiTrash2
+                              className="cursor-pointer"
+                              onClick={() => handleDeleteComment(comment.id)}
+                            />
+                          )}
                         </div>
-                      </>
+                        <div className="tracking-tighter">
+                          {comment.comment}
+                        </div>
+                        <div className="flex items-center w-full space-x-4">
+                          <SlHeart
+                            onClick={() => handleLikeComment(comment.id)}
+                            className="cursor-pointer"
+                            size={15}
+                          />
+                          <SlBubble size={15} />
+                        </div>
+                      </div>
                     );
                   })}
                 </div>
