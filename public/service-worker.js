4aec4de47fc1dc457fef076dac42d229ca080d7a
nitin <nimble2405@gmail.com>
2024-08-13 14:57:22 +0530

    minor changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,9 +13,25 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
+    }).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
+
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
